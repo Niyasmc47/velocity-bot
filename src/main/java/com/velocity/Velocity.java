4570806e20b90b636e904b046245c8f5c4f28e86
Niyasmc47 <niyasmc47@gmail.com>
2026-09-23 20:01:51 +0530

    Added an onReady EventListener

--- src/main/java/com/velocity/Velocity.java
+++ src/main/java/com/velocity/Velocity.java
@@ -2,13 +2,16 @@ package com.velocity;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import io.github.cdimascio.dotenv.Dotenv;
+import com.velocity.listener.ReadyListener;
 public class Velocity{
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
+        ReadyListener readyListener = new ReadyListener();
         System.out.println("Velocity is starting");
         String token = dotenv.get("DISCORD_TOKEN");
         JDA jda = JDABuilder
                     .createDefault(token)
+                    .addEventListeners(readyListener)
                     .build();
     }
 }
